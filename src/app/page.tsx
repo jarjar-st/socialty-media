3728b127cf34d4aaef39732066f170ce076a6339
jarjar-st <josue_benjamin12@hotmail.com>
2023-11-13 22:23:50 -0600

    button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,19 @@
 import Navbar from '@/components/Navbar'
 import React from 'react'
+import { buttonVariants } from "@/components/ui/button"
+import Link from 'next/link'
 
 function Home() {
   return (
     <div className='bg-[url(/banner-1.jpg)] h-[100vh] bg-right bg-no-repeat'>
       <Navbar/>
-      <div className=''>
-        <p>sdfsdfsdsf</p>
-        <p>sdfdsfsdf</p>
-        <p>sdfdsfsdf</p>
-        <p>sdfdsfsdf</p>
-        <p>sdfdsfsdf</p>
-        <p>sdfdsfsdf</p>
-        <p>sdfdsfsdf</p>
-        <p>sdfdsfsdf</p>
-        <p>sdfdsfsdf</p>
+      <div className='text-black text-6xl px-10 pt-12 font-bold'>
+        <p>Need a website</p>
+        <p>but dont&apos;t know</p>
+        <p>where to start?</p>
+        Link
       </div>
+      <Link href={"/"} className={buttonVariants({ variant: "outline" })}>Click here</Link>
     </div>
   )
 }
